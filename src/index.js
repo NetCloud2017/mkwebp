737ebfa40942f02c2f4d20ab0f8483165fd58643
DESKTOP-NCK2EPM\ironMax <tothinktodo@163.com>
2023-03-24 18:39:44 +0800

    采用 module asset 处理图片，处理 index 页面图片引用

--- src/index.js
+++ src/index.js
@@ -5,14 +5,3 @@ import "./js/jquery-1.12.4.min";
 import "./js/public";
 import "./js/nav";
 import "./js/jquery.flexslider-min";
-$(function () {
-  $("#home_slider").flexslider({
-    animation: "slide",
-    controlNav: true,
-    directionNav: true,
-    animationLoop: true,
-    slideshow: true,
-    slideshowSpeed: 2000,
-    useCSS: false,
-  });
-});
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,15 @@
 const { resolve } = require("path");
 const htmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+
 module.exports = {
   //   entry: "./index.js", // 不写名字默认识 main
   entry: { bundle: "./src/index.js" },
   mode: "development",
   output: {
-    filename: "./[hash:5][name].js",
+    filename: ".js/[hash:5][name].js",
     clean: true,
-    path: resolve(__dirname, "dist"),
+    path: resolve(__dirname, "dist/"),
   },
   module: {
     rules: [
@@ -16,14 +18,27 @@ module.exports = {
         use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(png|gif|svg|jpg|jpeg)$/,
+        test: /\.(png|gif|svg|jpg|jpeg)$/i,
+        type: "asset", // 表示采用 webpack 5 中内置的 asset 特性
+        parser: {
+          dataUrlCondition: {
+            maxSize: 8 * 1024, // 小于 8 kb 的就转base64
+          },
+        },
+        generator: {
+          filename: "image/[hash:6][name][ext]",
+        },
       },
     ],
   },
   plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+    }),
     new htmlWebpackPlugin({
-      filename: "home.html",
-      template: "./ZBestPC/index.html",
+      filename: "index.html",
+      template: "./src/index.html",
     }),
   ],
 };
